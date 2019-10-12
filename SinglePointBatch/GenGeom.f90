12fe2412448aaa6e5780b2387acb2edc60c7c0bc
YifanShenSZ <yshen57@jhu.edu>
2019-10-11 21:23:16 -0400

    Suit Columbus format

--- SinglePointBatch/GenGeom.f90
+++ SinglePointBatch/GenGeom.f90
@@ -18,17 +18,11 @@ program main
 !Initialize
     call BetterRandomSeed()
     open(unit=99,file='geom',status='old')!Read molecule detail
-        write(*,*)'geom format should be A2,I8,3F14.8,F14.8:'
-        write(*,*)'    2 space for element symbol'
-        write(*,*)'    8 space for integer element number'
-        write(*,*)'    3 * 14 space with 8 decimals for Cartesian coordinate in atomic unit'
-        write(*,*)'    14 space with 8 decimals for atomic mass in atomic mass unit'
-        write(*,*)'Some Columbus version may use different format, please correct it manually'
         NAtoms=0; do; read(99,*,iostat=i); if(i/=0) exit; NAtoms=NAtoms+1; end do; rewind 99
         allocate(ElementSymbol(NAtoms)); allocate(ElementNumber(NAtoms))
         allocate(r0(3*NAtoms)); allocate(mass(NAtoms))
         do i=1,NAtoms
-            read(99,'(A2,I8,3F14.8,F14.8)')ElementSymbol(i),ElementNumber(i),r0(3*i-2:3*i),mass(i)
+            read(99,*)ElementSymbol(i),ElementNumber(i),r0(3*i-2:3*i),mass(i)
             ElementSymbol(i)=trim(adjustl(ElementSymbol(i)))
         end do
         mass=mass*AMUInAU; call StandardizeGeometry(r0,mass,NAtoms,1)
@@ -43,7 +37,7 @@ open(unit=99,file='geom.all',status='replace')!Modification starts here
         q(1)=q(1)+dble(i)*0.04d0
         r=CartesianCoordinater(q,cartdim,intdim,mass=mass,r0=r0)
         do j=1,NAtoms
-            write(99,'(A2,I8,3F14.8,F14.8)')ElementSymbol(j),ElementNumber(j),r(3*j-2:3*j),mass(j)/AMUInAU
+            write(99,'(1x,A2,2x,F5.1,4F14.8)')ElementSymbol(j),dble(ElementNumber(j)),r(3*j-2:3*j),mass(j)/AMUInAU
         end do
     end do
 close(99)
