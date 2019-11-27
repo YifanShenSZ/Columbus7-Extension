7fd88ed7030c1b77656a799116f715ad6b012ac3
YifanShenSZ <yshen57@jhu.edu>
2019-11-26 19:05:29 -0500

    Remove unnecessary standardization

--- SinglePointBatch/loop.f90
+++ SinglePointBatch/loop.f90
@@ -15,16 +15,6 @@ program main
     character*32::chartemp; logical::flag; integer::i,j
     real*8,allocatable,dimension(:)::q,r
 !Initialize
-    open(unit=99,file='geom',status='old')!Read reference geometry
-        NAtoms=0; do; read(99,*,iostat=i); if(i/=0) exit; NAtoms=NAtoms+1; end do; rewind 99
-        allocate(ElementSymbol(NAtoms)); allocate(ElementNumber(NAtoms))
-        allocate(r0(3*NAtoms)); allocate(mass(NAtoms))
-        do i=1,NAtoms
-            read(99,*)ElementSymbol(i),ElementNumber(i),r0(3*i-2:3*i),mass(i)
-        end do
-        mass=mass*AMUInAU; call StandardizeGeometry(r0,mass,NAtoms,1)
-    close(99)
-    cartdim=3*NAtoms
     inquire(file='InternalCoordinateDefinition',exist=flag)
     if(flag) then
         chartemp=''
@@ -33,22 +23,29 @@ program main
         chartemp='Columbus7'
         call DefineInternalCoordinate(chartemp,intdim)
     end if
+    open(unit=99,file='geom',status='old')!Read reference geometry
+        NAtoms=0; do; read(99,*,iostat=i); if(i/=0) exit; NAtoms=NAtoms+1; end do; rewind 99
+        allocate(ElementSymbol(NAtoms)); allocate(ElementNumber(NAtoms))
+        allocate(r0(3*NAtoms)); allocate(mass(NAtoms))
+        do i=1,NAtoms
+            read(99,*)ElementSymbol(i),ElementNumber(i),r0(3*i-2:3*i),mass(i)
+        end do
+        mass=mass*AMUInAU
+    close(99)
+    cartdim=3*NAtoms
+    allocate(r(cartdim)); r=r0; call StandardizeGeometry(r0,mass,NAtoms,1,reference=r)
     allocate(q0(intdim)); q0=InternalCoordinateq(r0,intdim,cartdim)
 !Generate a loop around the reference geometry
-    allocate(q(intdim)); allocate(r(cartdim))
+    allocate(q(intdim))
     open(unit=99,file='geom.all',status='replace')
         !Example: start from q0, displace 1st internal coordinate
         do i=-5,5
             q=q0
-            q(1)=q(1)+dble(i)*0.04d0
-            call WriteGeom()
+            q(1)=q(1)+dble(i)*0.01d0
+            r=CartesianCoordinater(q,cartdim,intdim,mass=mass,r0=r0)
+            do j=1,NAtoms
+                write(99,'(1x,A2,2x,F5.1,4F14.8)')ElementSymbol(j),ElementNumber(j),r(3*j-2:3*j),mass(j)/AMUInAU
+            end do
         end do
     close(99)
-    contains
-    subroutine WriteGeom()
-        r=CartesianCoordinater(q,cartdim,intdim,mass=mass,r0=r0)
-        do j=1,NAtoms
-            write(99,'(1x,A2,2x,F5.1,4F14.8)')ElementSymbol(j),ElementNumber(j),r(3*j-2:3*j),mass(j)/AMUInAU
-        end do
-    end subroutine WriteGeom
 end program main
--- SinglePointBatch/path.f90
+++ SinglePointBatch/path.f90
@@ -1,6 +1,6 @@
 !Generate a linear path from geometry 1 to geometry 2
 !Number of steps along the path is controled by NStep
-!Input : internal coordinate definition, geom1, geom2
+!Input : internal coordinate definition, geom1, geom2, (optional) NStep
 !Output: geom.all
 !Dependency: my Fortran-Library, as written in makefile
 program main
@@ -13,22 +13,12 @@ program main
 !Molecule information
     integer::NAtoms,intdim,cartdim
     character*2,allocatable,dimension(:)::ElementSymbol
-    real*8,allocatable,dimension(:)::ElementNumber,mass,r0,q0,r1,q1
+    real*8,allocatable,dimension(:)::ElementNumber,mass,r1,q1,r2,q2
 !Work variable
     character*32::chartemp; logical::flag; integer::i,j
     real*8::dbletemp,dbletemp1
-    real*8,allocatable,dimension(:)::q,r,dq
+    real*8,allocatable,dimension(:)::q,dq,r,rsave
 !Initialize
-    open(unit=99,file='geom1',status='old')!Read geometry 1
-        NAtoms=0; do; read(99,*,iostat=i); if(i/=0) exit; NAtoms=NAtoms+1; end do; rewind 99
-        allocate(ElementSymbol(NAtoms)); allocate(ElementNumber(NAtoms))
-        allocate(r0(3*NAtoms)); allocate(mass(NAtoms))
-        do i=1,NAtoms
-            read(99,*)ElementSymbol(i),ElementNumber(i),r0(3*i-2:3*i),mass(i)
-        end do
-        mass=mass*AMUInAU; call StandardizeGeometry(r0,mass,NAtoms,1)
-    close(99)
-    cartdim=3*NAtoms
     inquire(file='InternalCoordinateDefinition',exist=flag)
     if(flag) then
         chartemp=''
@@ -37,29 +27,47 @@ program main
         chartemp='Columbus7'
         call DefineInternalCoordinate(chartemp,intdim)
     end if
-    allocate(q0(intdim)); q0=InternalCoordinateq(r0,intdim,cartdim)
-    open(unit=99,file='geom2',status='old')!Read geometry 2
-        allocate(r1(3*NAtoms))
+    open(unit=99,file='geom1',status='old')!Read geometry 1
+        NAtoms=0; do; read(99,*,iostat=i); if(i/=0) exit; NAtoms=NAtoms+1; end do; rewind 99
+        allocate(ElementSymbol(NAtoms)); allocate(ElementNumber(NAtoms))
+        allocate(r1(3*NAtoms)); allocate(mass(NAtoms))
         do i=1,NAtoms
-            read(99,*)chartemp,dbletemp,r1(3*i-2:3*i),dbletemp1
+            read(99,*)ElementSymbol(i),ElementNumber(i),r1(3*i-2:3*i),mass(i)
         end do
-        call StandardizeGeometry(r1,mass,NAtoms,1)
+        mass=mass*AMUInAU
     close(99)
+    cartdim=3*NAtoms
+    allocate(r(cartdim)); r=r1; call StandardizeGeometry(r1,mass,NAtoms,1,reference=r)
     allocate(q1(intdim)); q1=InternalCoordinateq(r1,intdim,cartdim)
-!Generate a linear path from geometry 1 to geometry 2
-    allocate(q(intdim)); allocate(r(cartdim))
-    allocate(dq(intdim)); dq=(q1-q0)/dble(NStep+1)
-    open(unit=99,file='geom.all',status='replace')
-        do i=1,NStep
-            q=q0+dq*dble(i)
-            call WriteGeom()
+    open(unit=99,file='geom2',status='old')!Read geometry 2
+        allocate(r2(cartdim))
+        do i=1,NAtoms
+            read(99,*)chartemp,dbletemp,r2(3*i-2:3*i),dbletemp1
         end do
     close(99)
-    contains
-    subroutine WriteGeom()
-        r=CartesianCoordinater(q,cartdim,intdim,mass=mass,r0=r0)
-        do j=1,NAtoms
-            write(99,'(1x,A2,2x,F5.1,4F14.8)')ElementSymbol(j),ElementNumber(j),r(3*j-2:3*j),mass(j)/AMUInAU
+    allocate(q2(intdim)); q2=InternalCoordinateq(r2,intdim,cartdim)
+    inquire(file='NStep',exist=flag)
+    if(flag) then
+        open(unit=99,file='NStep',status='old'); read(99,*)NStep; close(99)
+    end if
+!Generate a linear path from geometry 1 to geometry 2
+    allocate(q(intdim))
+    allocate(dq(intdim)); dq=(q2-q1)/dble(NStep+1)
+    allocate(rsave(cartdim))
+    open(unit=99,file='geom.all',status='replace')
+            q=q1+dq
+            r=CartesianCoordinater(q,cartdim,intdim,mass=mass,r0=r1)
+            do j=1,NAtoms
+                write(99,'(1x,A2,2x,F5.1,4F14.8)')ElementSymbol(j),ElementNumber(j),r(3*j-2:3*j),mass(j)/AMUInAU
+            end do
+            rsave=r
+        do i=2,NStep
+            q=q1+dq*dble(i)
+            r=CartesianCoordinater(q,cartdim,intdim,mass=mass,r0=rsave)
+            do j=1,NAtoms
+                write(99,'(1x,A2,2x,F5.1,4F14.8)')ElementSymbol(j),ElementNumber(j),r(3*j-2:3*j),mass(j)/AMUInAU
+            end do
+            rsave=r
         end do
-    end subroutine WriteGeom
+    close(99)
 end program main
