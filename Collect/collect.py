ebf412651e21d5fd25f996cd7e8294ef49d03e3d
YifanShenSZ <yshen57@jhu.edu>
2020-01-22 13:36:11 -0500

    Remove internal coordinate gradient collection

--- Collect/collect.py
+++ Collect/collect.py
@@ -13,7 +13,6 @@ import numpy
 ''' Global variable '''
 NData  = 0 # Number of data points
 NAtoms = 0 # Number of atoms
-intdim = 0 # Internal coordinate dimension
 
 ''' Routine '''
 def parse_args() -> argparse.Namespace: # Command line input
@@ -24,7 +23,6 @@ def parse_args() -> argparse.Namespace: # Command line input
     parser.add_argument('NState',type=int,help='collect from state 1 to NState')
     parser.add_argument('-s','--single', action='store_true',help='only collect NState-th state')
     parser.add_argument('-e','--energy' ,action='store_true',help='only collect energy')
-    parser.add_argument('-i','--intgrad',action='store_true',help='additionally collect internal coordinate gradient')
     parser.add_argument('-m','--mcscf',  action='store_true',help='collect MCSCF result rather than MRCI')
     args = parser.parse_args()
     return args
@@ -33,7 +31,6 @@ def SingleState(args: argparse.Namespace):
     # Allocate memory
     energy=numpy.empty(NData)
     cartgrad=numpy.empty((NData,NAtoms,3))
-    if args.intgrad: intgrad=numpy.empty((NData,intdim))
     if args.energy: # only collect energy
         # Read
         for i in range(args.StartDirectory,args.EndDirectory+1):
@@ -65,25 +62,17 @@ def SingleState(args: argparse.Namespace):
                 for j in range(NAtoms):
                     temp=lines[j].split()
                     for k in range(3): cartgrad[i,j,k]=float(temp[k].replace('D','e'))
-                if args.intgrad: # internal coordinate gradient
-                    with open(args.BatchPath/str(i)/'GRADIENTS'/('intgrd.drt1.state'+str(args.NState)+'.sp'),'r') as f: lines=f.readlines()
-                    for j in range(intdim): intgrad[i,j]=float(lines[j].replace('D','e'))
         # Output
         with open('energy.all','w') as f: # energy
             for i in range(NData): print(energy[i],file=f)
         with open('cartgrd.drt1.state'+str(args.NState)+'.all','w') as f: # gradient
             for i in range(NData):
                 for j in range(NAtoms): print(cartgrad[i,j,0],cartgrad[i,j,1],cartgrad[i,j,2],sep=' ',file=f)
-        if args.intgrad: # internal coordinate gradient
-            with open('intgrd.drt1.state'+str(args.NState)+'.all','w') as f:
-                for i in range(NData):
-                    for j in range(intdim): print(intgrad[i,j],file=f)
 
 def MultiState(args: argparse.Namespace):
     # Allocate memory
     energy=numpy.empty((NData,args.NState))
     cartgrad=numpy.empty((NData,args.NState,args.NState,NAtoms,3))
-    if args.intgrad: intgrad=numpy.empty((NData,args.NState,args.NState,intdim))
     if args.energy: # only collect energy
         # Read
         for i in range(args.StartDirectory,args.EndDirectory+1):
@@ -125,13 +114,6 @@ def MultiState(args: argparse.Namespace):
                         for j in range(NAtoms):
                             temp=lines[j].split()
                             for k in range(3): cartgrad[i,istate,jstate,j,k]=float(temp[k].replace('D','e'))
-                if args.intgrad: # internal coordinate gradient
-                    for istate in range(args.NState):
-                        with open(args.BatchPath/str(i)/'GRADIENTS'/('intgrd.drt1.state'+str(istate+1)+'.sp'),'r') as f: lines=f.readlines()
-                        for j in range(intdim): intgrad[i,istate,istate,j]=float(lines[j].replace('D','e'))
-                        for jstate in range(istate+1,args.NState):
-                            with open(args.BatchPath/str(i)/'GRADIENTS'/('intgrd.nad.drt1.state'+str(istate+1)+'.drt1.state'+str(jstate+1)+'.sp'),'r') as f: lines=f.readlines()
-                            for j in range(intdim): intgrad[i,istate,jstate,j]=float(lines[j].replace('D','e'))
         # Output
         with open('energy.all','w') as f: # energy
             for i in range(NData):
@@ -146,18 +128,8 @@ def MultiState(args: argparse.Namespace):
                     for i in range(NData):
                         cartgrad[i,istate,jstate,:,:]=cartgrad[i,istate,jstate,:,:]*(energy[i,jstate]-energy[i,istate])
                         for j in range(NAtoms): print(cartgrad[i,istate,jstate,j,0],cartgrad[i,istate,jstate,j,1],cartgrad[i,istate,jstate,j,2],sep=' ',file=f)
-        if args.intgrad: # internal coordinate gradient
-            for istate in range(args.NState):
-                with open('intgrd.drt1.state'+str(istate+1)+'.all','w') as f:
-                    for i in range(NData):
-                        for j in range(intdim): print(intgrad[i,istate,istate,j],file=f)
-                for jstate in range(istate+1,args.NState):
-                    with open('intgrd.nad.drt1.state'+str(istate+1)+'.drt1.state'+str(jstate+1)+'.all','w') as f:
-                        for i in range(NData):
-                            intgrad[i,istate,jstate,:]=intgrad[i,istate,jstate,:]*(energy[i,jstate]-energy[i,istate])
-                            for j in range(intdim): print(intgrad[i,istate,jstate,j],file=f)
 
-def mcscf(args: argparse.Namespace):
+def mcscf(args: argparse.Namespace): # Currently, energy only
     # Allocate memory
     energy=numpy.empty((NData,args.NState))
     # Read energy
@@ -182,9 +154,6 @@ if __name__ == "__main__":
     NData = args.EndDirectory - args.StartDirectory + 1
     with open(args.BatchPath/str(args.StartDirectory)/'geom','r') as f: # Get NAtoms
         NAtoms=len(f.readlines())
-    if args.intgrad: # Get intdim
-        with open(args.BatchPath/str(args.StartDirectory)/'GRADIENTS'/('intgrd.drt1.state'+str(args.NState)+'.sp'),'r') as f:
-            intdim=len(f.readlines())
     # Do the job
     if args.mcscf:
         mcscf(args)
