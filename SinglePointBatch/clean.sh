8fa399ddb0baa14bf965e1766b48ec7a28752773
YifanShenSZ <yshen57@jhu.edu>
2020-03-06 21:33:52 -0500

    More to clean

--- SinglePointBatch/clean.sh
+++ SinglePointBatch/clean.sh
@@ -21,14 +21,28 @@ done
 # Do the job
 for entry in * ; do
     if [ -d $entry ]; then # This is a directory
-        cd $entry
+    cd $entry
         if [ -f 'runc.log' ]; then # This directory has a started job
             success=`cat runc.log |grep timings`
             if [ -n "$success" ]; then # This job has succeeded
-                rm *
+            mkdir clean_back_up
+                mv geom clean_back_up
+                    rm *
+                mv clean_back_up/geom .
                 rm -r COSMO MOCOEFS MOLDEN RESTART
+                cd LISTINGS
+                    mv trncils* ../clean_back_up
+                        rm *ls*
+                    mv ../clean_back_up/* .
+                    rm *energy*
+                cd ..
+                cd GRADIENTS
+                    rm intgrd.nad*
+                    rm nadxfl*
+                cd ..
+            rm -r clean_back_up
             fi
         fi
-        cd ..
+    cd ..
     fi
 done
