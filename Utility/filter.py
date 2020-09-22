faa6224bda632c720a780e5a2a8f27139a6662ac
YifanShenSZ <yshen57@jhu.edu>
2020-09-22 09:40:12 -0400

    add sanity check

--- Utility/filter.py
+++ Utility/filter.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     args = parse_args()
     ''' Do the job '''
     with open(args.geom_data, 'r') as f: lines = f.readlines()
+    assert len(lines[0]) > 52, "This geom data file has already been filtered"
     with open(args.geom_data, 'w') as f:
         for line in lines:
             print(line[:2], line[10:52], sep='', file=f)
